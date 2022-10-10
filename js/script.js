69f8a38b316542b4830c3259b230800df653635d
Anderson Alves <54083111+napunda@users.noreply.github.com>
2022-10-10 18:26:37 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function ModalClose() {
+  const modal = document.querySelector(".modal");
+  modal.classList.toggle("modal-hide");
+}
+function ModalOpen() {
+  const modal = document.querySelector(".modal");
+  modal.classList.toggle("modal-hide");
+}

09357ea08362cc5123e062a380850e09aca4d8d8
napund4 <napunda@mail.com>
2022-10-25 20:39:32 -0300

    sync

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const transactions = [
   {
     id: 1,
     description: "Hamburguer",
-    amount: -59,
+    amount: -59.5,
     date: "10/04/2020",
   },
   {
@@ -32,14 +32,40 @@ const transactions = [
   },
 ];
 
-function pushTransaction(transaction) {
-  const trigger = document.querySelector("#trigger");
-}
-
 const Transaction = {
-  incomes() {},
-  outflow() {},
-  balance() {},
+  incomes() {
+    let incomes = 0;
+    transactions.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        incomes += transaction.amount;
+      }
+    });
+    return incomes;
+  },
+
+  outflow() {
+    let outflow = 0;
+    transactions.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        outflow += transaction.amount;
+      }
+    });
+    return outflow * -1;
+  },
+
+  balance() {
+    return Number(Transaction.incomes() - Transaction.outflow());
+  },
+
+  add(transaction) {
+    transactions.push(transaction);
+    App.reload();
+  },
+
+  remove(index) {
+    transactions.splice(index, 1);
+    App.reload();
+  },
 };
 
 const insertHTML = {
@@ -54,20 +80,85 @@ const insertHTML = {
     const html = `
     <tr>
       <th>${transaction.description}</th>
-      <td class="transactions-incomes">${transaction.amount.toLocaleString(
-        "pt-br",
-        {
-          style: "currency",
-          currency: "BRL",
-        }
-      )}</td>
+      <td class="${
+        transaction.amount > 0
+          ? `transactions-incomes`
+          : `transactions-outflow `
+      }">${transaction.amount.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    })}</td>
       <td>${transaction.date}</td>
       <th><i class="fas fa-minus-circle"></i></th>
     </tr>`;
     return html;
   },
+
+  displayBalance() {
+    const balance = Transaction.balance().toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+    const incomes = Transaction.incomes().toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+    const outflow = Transaction.outflow().toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+    document.getElementById("displayBalance").innerHTML = balance;
+    document.getElementById("displayincomes").innerHTML = incomes;
+    document.getElementById("displayOutflow").innerHTML = outflow;
+  },
+  cleartransaction() {
+    insertHTML.transactionsContainer.innerHTML = "";
+  },
 };
 
-transactions.forEach(function (transaction) {
-  insertHTML.addTransaction(transaction);
-});
+const App = {
+  init() {
+    transactions.forEach(function (transaction) {
+      insertHTML.addTransaction(transaction);
+    });
+
+    insertHTML.displayBalance();
+  },
+
+  reload() {
+    insertHTML.cleartransaction();
+    App.init();
+  },
+};
+
+const Form = {
+  description: document.getElementById("modalDescription"),
+  amount: document.getElementById("modalValue"),
+  date: document.getElementById("modalDate"),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+
+  validatFields() {
+    const { description, amount, date } = Form.getValues();
+
+    if (description.trim() === "" || amount.trim() == "" || date.trim() == "") {
+      throw new Error("Preencha todos os campos.");
+    }
+  },
+  submit() {
+    try {
+      Form.validatFields();
+      console.log(Form.getValues());
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
+
+App.init();
